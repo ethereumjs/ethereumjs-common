40193360492f614aaae07f061793b750b0abdbbd
Erin Shaben <eshaben@icloud.com>
2019-01-16 17:28:30 -0800

    playing around with constantinople-1283-removed flag

--- dist/hardforks/index.js
+++ dist/hardforks/index.js
@@ -8,5 +8,6 @@ exports.hardforks = [
     ['spuriousDragon', require('./spuriousDragon.json')],
     ['byzantium', require('./byzantium.json')],
     ['constantinople', require('./constantinople.json')],
+    ['constantinople-1283-removed', require('./constantinople-1283-removed.json')],
 ];
 //# sourceMappingURL=index.js.map
