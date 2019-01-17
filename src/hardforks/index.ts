9ce41df2e279e8fb27233d315391fe03acf4927b
Erin Shaben <eshaben@icloud.com>
2019-01-16 17:26:57 -0800

    playing around with constantinople-1283-removed flag

--- src/hardforks/index.ts
+++ src/hardforks/index.ts
@@ -6,4 +6,5 @@ export const hardforks = [
   ['spuriousDragon', require('./spuriousDragon.json')],
   ['byzantium', require('./byzantium.json')],
   ['constantinople', require('./constantinople.json')],
+  ['constantinople-1283-removed', require('./constantinople-1283-removed.json')],
 ]
